ea1a2335831f31a5a4bff143ca91f81233019ef6
c0nf1dxnt <c0nf1dxnt@yandex.ru>
2023-09-04 23:24:03 +0300

    11:23 PM 9/4/2023 version

--- homework02092023/Program.cs
+++ homework02092023/Program.cs
@@ -14,31 +14,45 @@ namespace homework02092023
         static void task1()
         {
             Console.WriteLine("Task1:");
+            Console.WriteLine("Задание: Вывод трёх строк в указанном в файле с домашним заданием порядке");
             Console.WriteLine("a. Мир Труд Май");
             Console.WriteLine("b. Мир");
             Console.WriteLine("     Труд");
-            Console.WriteLine("         Май");
+            Console.WriteLine("         Май\n");
+        }
+        static void task1ver2()
+        {
+            Console.WriteLine("Task1:");
+            Console.WriteLine("Задание: Вывод трёх строк в указанном в файле с домашним заданием порядке");
+            Console.WriteLine("a. Мир Труд Май");
+            Console.WriteLine("b. Мир\n\tТруд\n\t\tМай\n");
         }
         static void task2()
         {
             Console.WriteLine("Task2:");
+            Console.WriteLine("Задание: ");
         }
         static void task3()
         {
             Console.WriteLine("Task3:");
+            Console.WriteLine("Задание: Считать с клавиатуры радиус и вывести длину соответствующей окружности и площадь круга");
             Console.Write("Радиус окружности равен: ");
             double radius = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Длина окружности равна: " + 2 * radius * Math.PI);
-            Console.WriteLine("Площадь круга равна: " + Math.Pow(radius,2) * Math.PI);
+            Console.WriteLine("Площадь круга равна: " + Math.Pow(radius,2) * Math.PI + "\n");
         }
         static void task4()
         {
             Console.WriteLine("Task4:");
+            Console.WriteLine("Задание: Найти значение y = cos(x), считать x с клавиатуры");
+            Console.Write("x = ");
+            Console.WriteLine("y = " + Math.Cos(Convert.ToDouble(Console.ReadLine())));
         }
 
         static void task5()
         {
             Console.WriteLine("Task5:");
+            Console.WriteLine("Задание: Считать коэф. кв. ур. с клавиатуры и найти корни кв. ур.");
             Console.Write("Введите коэффициент \"a\" квадратного уравнения: ");
             int a = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите коэффициент \"b\" квадратного уравнения: ");
@@ -46,18 +60,19 @@ namespace homework02092023
             Console.Write("Введите коэффициент \"c\" квадратного уравнения: ");
             int c = Convert.ToInt32(Console.ReadLine());
 
-            //int discriminant = Math.Pow(b, 2) - 4 * a * c;
             if (Math.Pow(b, 2) - 4 * a * c < 0)
             {
                 Console.WriteLine("Корней нет");
             }
             else if (Math.Pow(b, 2) - 4 * a * c == 0)
             {
-                Console.Write("Один корень, x = " + (-b / (2 * a)));
+                Console.WriteLine("Один корень");
+                Console.WriteLine("x = " + (-b / (2 * a)));
             }
             else
             {
-                Console.WriteLine("Два корня, x1 = " +  ((-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a)));
+                Console.WriteLine("Два корня:");
+                Console.WriteLine("x1 = " + (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a));
                 Console.WriteLine("x2 = " + ((-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a)));
             }
 
@@ -71,15 +86,13 @@ namespace homework02092023
         static void task7()
         {
             Console.WriteLine("Task7:");
+            Console.WriteLine("Задание: Вывести столбиком 4 случайных числа");
             Random random = new Random();
 
-            int range1 = -2147483648;
-            int range2 = 2137483647;
-
-            Console.WriteLine("Случайное число №1 " + random.Next(range1,range2));
-            Console.WriteLine("Случайное число №2 " + random.Next(range1, range2));
-            Console.WriteLine("Случайное число №3 " + random.Next(range1, range2));
-            Console.WriteLine("Случайное число №4 " + random.Next(range1, range2));
+            Console.WriteLine("Случайное число №1 " + random.Next());
+            Console.WriteLine("Случайное число №2 " + random.Next());
+            Console.WriteLine("Случайное число №3 " + random.Next());
+            Console.WriteLine("Случайное число №4 " + random.Next());
         }
         static void task8()
         {
@@ -95,20 +108,31 @@ namespace homework02092023
         static void task9()
         {
             Console.WriteLine("Task9:");
+            Console.WriteLine("Задание: Считать с клавиатуры стоимость 1 кг конфет, печенья, яблок и вес купленных конфет, печенья и яблок, а затем вывести стоимость покупки");
+            double priceOfCandy = Convert.ToDouble(Console.ReadLine());
+            double priceOfCookies = Convert.ToDouble(Console.ReadLine());
+            double priceOfApple = Convert.ToDouble(Console.ReadLine());
+
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(priceOfCandy * x + priceOfCookies * y + priceOfApple * z);
         }
 
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
 
-            task1();
-            task2();
-            task3();
-            task4();
-            task5();
-            task6();
-            task7();
-            task8();
+            //task1();
+            //task1ver2();
+            //task2();
+            //task3();
+            //task4();
+            //task5();
+            //task6();
+            //task7();
+            //task8();
             task9();
             Console.ReadKey();
         }
