c0fde356e1c602b4b0d5a5a807f00d990e90e012
c0nf1dxnt <c0nf1dxnt@yandex.ru>
2023-09-21 15:33:46 +0300

    3:33 PM 9/21/2023 version

---
+++ homework16092023/Part3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Part3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
